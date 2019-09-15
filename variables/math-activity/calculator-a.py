2c93e7074298b42ca5097761d5b03d8efb4fa6c7
Amanda Bertsch <abertsch72@gmail.com>
2019-09-15 15:08:26 -0700

    stub out math activities

---
+++ variables/math-activity/calculator-a.py
@@ -0,0 +1,24 @@
+"""
+Let's make a simple calculator that can complete several tasks!
+We'll ask the user for 2 numbers and then do a bunch of operations.
+
+Fill in the #TODO statements to prompt the user and print out the results!
+"""
+
+print("Welcome to the calculator!")
+
+# TODO: get these numbers by asking the user for input!
+num1 = 1
+num2 = 1
+
+print(str(num1) + " + " + str(num2) + " = ")
+
+print(str(num1) + " - " + str(num2) + " = ")
+
+print(str(num1) + " * " + str(num2) + " = ")
+
+print(str(num1) + " / " + str(num2) + " = ")
+
+print("The remainder when you divide " + str(num1) + " by " + str(num2) + " is ")
+
+
