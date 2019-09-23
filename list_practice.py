a6650b81caba3c8585a1327f3490793b7ddc890b
Annie Gao <anniegao@email.arizona.edu>
2019-09-22 20:35:39 -0700

    list practice and math practice activities

---
+++ list_practice.py
@@ -0,0 +1,52 @@
+# Contributor: Annie Gao
+# Created: 9/21/2019
+# Last edited: 9/21/2019
+# Practice with lists
+
+coffee = ["water", "coffee beans", "time", "heat"]
+
+# How do we get "time" out of the coffee list? Print it out using indexing. 
+
+
+
+
+# How do we add "milk"? Add it to the coffee list, then print out the list. 
+
+
+
+# Never mind, I don't want to add milk. I want to add sugar. How do I change the list?
+# Print out the list result.
+
+
+
+
+# I want a cold brew. This doesn't use heat. Print out the list. Add and remove whatever you like
+# in a coffee!
+
+
+
+
+
+
+
+# Make at least three sentences out of the given list by only adding things from the list by indexing. 
+# Hint: There is a way to find the index of an item in a list. See if you can Google for the answer!
+
+sentence_making = ["chair", "market", "his", "at", "pushes", "open", "nighttime", " ", ",", 
+                    "fruit", "to", "spilled", "is", ".", "laptop", "ground","that", "onto", "late", 
+                    "games", "there", "what", "come", "how", "where", "some", "the", "exit",
+                    "backpack", "ran",  "small", "car", "broken", "closed", "close", "later"]
+
+
+
+
+
+
+
+
+
+# Lists have a lot of other functions. If you have time, ask a facilitator for functions like pop(), 
+# reverse(), insert(), etc.
+
+
+
---
+++ math_practice.py
@@ -0,0 +1,47 @@
+# Contributor: Annie Gao
+# Created: 9/21/2019
+# Last edited: 9/21/2019
+# Math practice:
+
+# Problem 1: How do we get the string "True or False?"
+# Hint: Don't forget about casting! 
+# Print your result to the console (the page to the right) by using print statements.
+
+something = "True"
+something_else = False
+
+
+
+
+# Problem 2: What is the result of adding the variables below, in order from top to bottom?
+# Print out the result.
+
+car = "water"
+wheel = "12"
+garden = " flowers"
+
+
+
+
+
+# Problem 3: How can we get it to print the variable below 20 times, in one string?
+
+forever = "100"
+
+
+
+# Problem 4: What if we change the variable above to:
+
+new_forever = int(forever)
+
+# What happens when we use the same format as Problem 3 to try printing it 20 times? 
+
+
+
+# Problem 5: Quick maths -- what's 432 * 12 + 99? 
+
+
+
+
+# Try casting, adding, multiplying, and printing the variables in any of the problems 
+# to any other type of variable. What happens if we cast a string to a boolean? 
