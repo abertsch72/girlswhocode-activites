f977645f55f19ba1d446882721b284f2e9b9d258
Amanda Bertsch <abertsch72@gmail.com>
2019-10-01 08:10:08 -0700

    adds -a activity, starts -b for conditionals week 2

---
+++ conditionals/week2-activities/lunch-seats-b.py
@@ -0,0 +1,17 @@
+"""
+This is a logic puzzle game
+"""
+# A B C D
+
+# C B A D
+
+# B C D A
+
+# D A B C
+
+# C picks a seat
+
+
+# B wants to sit next to C but never next to D
+
+# A wants to sit on the end, unless that would mean B has to sit next to D
